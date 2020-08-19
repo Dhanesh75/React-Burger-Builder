460097c7ca217d4da9221ad3e5fc967157099f9d
Dhanesh <dhaneshwalte7@gmail.com>
2020-08-19 20:23:12 +0530

    Added Fill Form Text

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./CheckoutSummary.module.css";
 import Burger from "../../Burger/Burger";
 import Button from "../../UI/Button/Button";
 
 const CheckoutSummary = (props) => {
+
+  const [headerText, headerTextChanged] = useState(<h1>This Looks Yum!</h1>);
+
+  const onCheckoutClicked = () => {
+    headerTextChanged(<h1>Fill out the contact form below</h1>)
+    props.onCheckoutContinued();
+  }
+
   return (
     <div className={classes.CheckoutSummary}>
-      <h1>This looks Yum!</h1>
+      {headerText}
       <div style={{ width: "100%", margin: "auto" }}>
         <Burger ingredients={props.ingredients} />
         <Button btnType="Danger" clicked={props.onCheckoutCancelled}>
           Cancel
         </Button>
-        <Button btnType="Success" clicked={props.onCheckoutContinued}>
+        <Button btnType="Success" clicked={onCheckoutClicked}>
           Continue
         </Button>
       </div>
